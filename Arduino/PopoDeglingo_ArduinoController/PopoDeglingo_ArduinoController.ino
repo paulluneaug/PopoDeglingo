903064a005e3d306a0c558d3a5f92ed6fdb8c422
p.luneau <paul.luneau@mail.com>
2025-09-02 16:22:02 +0200

    The arduino is a joystick

--- Arduino/PopoDeglingo_ArduinoController/PopoDeglingo_ArduinoController.ino
+++ Arduino/PopoDeglingo_ArduinoController/PopoDeglingo_ArduinoController.ino
@@ -1,9 +1,17 @@
-void setup() {
+#include <Joystick.h>
+
+
+Joystick_ m_joystick {};
+
+void setup() 
+{
   // put your setup code here, to run once:
+  m_joystick.begin(); 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  m_joystick.sendState();
 }
